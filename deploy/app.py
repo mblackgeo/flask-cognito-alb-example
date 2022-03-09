c1e6dfd62d6fbb6206976807e4b9d31d29c61aeb
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-09 14:59:12 +0000

    Add initial stack and basic changes for cdk v2

--- deploy/app.py
+++ deploy/app.py
@@ -1,10 +1,11 @@
 import aws_cdk as cdk
 
 from stacks.config import cfg
+from stacks.ecs_cognito_stack import ECSCognitoStack
 
 app = cdk.App()
 cdk_env = cdk.Environment(region=cfg.AWS_REGION, account=cfg.AWS_ACCOUNT)
 
-# TODO stacks
+ECSCognitoStack(app, f"{cfg.NAMESPACE}-stack", env=cdk_env)
 
 app.synth()
