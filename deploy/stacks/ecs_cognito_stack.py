30707764e8d39534471bd4406eac6e44ec5dc671
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-09 15:34:14 +0000

    Add TODOs

--- deploy/stacks/ecs_cognito_stack.py
+++ deploy/stacks/ecs_cognito_stack.py
@@ -17,7 +17,7 @@ from stacks.config import cfg
 class ECSCognitoStack(core.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
+        # TODO split into separate stacks
         # ---------------------------------------------------------------------
         # VPC / ECS setup
         # ---------------------------------------------------------------------
