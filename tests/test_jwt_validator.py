2a1e4766b480f5b57bb21caaaa4b15b8c1f0908e
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-11 15:50:15 +0000

    Simplify creation of JWT in test

--- tests/test_jwt_validator.py
+++ tests/test_jwt_validator.py
@@ -5,22 +5,20 @@ import jwt
 from webapp.auth.jwt_validator import JWTValidator
 
 
-def make_jwt(header=None, claims=None) -> str:
-    h = {
-        "kid": "test",
-    }
-    if header:
-        h.update(header)
+def make_jwt(expiry_time=None) -> str:
+    if expiry_time is None:
+        expiry_time = int(datetime.now().timestamp()) + 600
 
-    c = {
-        "iss": "767676",
-        "aud": "test",
-        "exp": int(datetime.now().timestamp()) + 600,
-    }
-    if claims:
-        c.update(claims)
-
-    return jwt.encode(payload=c, headers=h, key="secret", algorithm="HS256")
+    return jwt.encode(
+        payload={
+            "exp": expiry_time,
+        },
+        headers={
+            "kid": "test",
+        },
+        key="secret",
+        algorithm="HS256",
+    )
 
 
 def test_jwt_validator_valid_key(mocker):
@@ -32,19 +30,10 @@ def test_jwt_validator_valid_key(mocker):
     assert JWTValidator(token).is_valid()
 
 
-def test_jwt_validator_wrong_aud(mocker):
-    mocker.patch(
-        "webapp.auth.jwt_validator.JWTValidator.get_public_key", return_value="secret"
-    )
-
-    token = make_jwt(claims={"aud": "wrong"})
-    assert not JWTValidator(token).is_valid()
-
-
 def test_jwt_validator_expired(mocker):
     mocker.patch(
         "webapp.auth.jwt_validator.JWTValidator.get_public_key", return_value="secret"
     )
 
-    token = make_jwt(claims={"exp": int(datetime.now().timestamp()) - 600})
+    token = make_jwt(expiry_time=int(datetime.now().timestamp()) - 600)
     assert not JWTValidator(token).is_valid()
